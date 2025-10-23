12c4772bc2eff48a3a2e2bac57c680376e3a9dcb
tetrasurfaces <sendtotoddhutchinson@gmail.com>
2025-10-23 22:42:00 +0900

    Update kappa_wire.py

--- src/hash/kappa_wire.py
+++ src/hash/kappa_wire.py
@@ -18,7 +18,7 @@
 
 import numpy as np
 import asyncio
-from spiral_hash import kappa_spiral_hash  # Import spiral hash
+from src.hash.spiral_hash import kappa_spiral_hash  # Import spiral hash
 
 class KappaWire:
     def __init__(self, grid_size=107):
