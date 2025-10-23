561eb8ca83e31533ff8d545af0083b038ae5ebba
tetrasurfaces <sendtotoddhutchinson@gmail.com>
2025-10-23 11:24:24 +0900

    Update miracle_endian.py

--- src/core/miracle_endian.py
+++ src/core/miracle_endian.py
@@ -1,6 +1,26 @@
-# KappashaOS/core/tree/miracle_endian.py
-# AGPL-3.0-or-later, xAI fork 2025
+# License: AGPL-3.0-or-later (xAI fork, 2025)
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+# Copyright 2025 xAI
+#
+# Private Development Note: This repository is private for xAI’s KappashaOS development.
+# Access restricted until public phase. Consult tetrasurfaces (github.com/tetrasurfaces/issues) post-release.
+
+# KappashaOS/src/core/miracle_endian.py
+# License: AGPL-3.0-or-later (xAI fork, 2025)
 # No warranties. See <https://www.gnu.org/licenses/>.
+
 
 import numpy as np
 
