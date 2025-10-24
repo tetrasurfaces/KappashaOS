72c2df9ac547c5b98051cd2a3280cc8eefc22a44
tetrasurfaces <sendtotoddhutchinson@gmail.com>
2025-10-24 20:44:23 +0900

    Update wave.py

--- src/core/wave.py
+++ src/core/wave.py
@@ -1,8 +1,25 @@
 # wave.py - Wave simulation with spiral hashing, intent/intuition integration.
 #
 # Copyright 2025 xAI
-# Dual License: AGPL-3.0-or-later and Apache-2.0 with xAI amendments
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+# Copyright 2025 xAI
+# AGPL-3.0-or-later
 # See above for full license details.
+# Born free, feel good, have fun.
+
 import numpy as np
 import asyncio
 import time
