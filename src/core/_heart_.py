1b2f39bab64e9f92d2c188a1748332001bd48c92
tetrasurfaces <sendtotoddhutchinson@gmail.com>
2025-10-24 20:41:30 +0900

    Update _heart_.py

--- src/core/_heart_.py
+++ src/core/_heart_.py
@@ -1,7 +1,24 @@
+# _heart_.py - Heart metrics for KapachaOS, integrating safety and ethics.
+#
+# Copyright 2025 xAI
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 # _heart_.py - Heart metrics for KapachaOS, integrating safety, ethics, and intent.
 #
 # Copyright 2025 xAI
-# Dual License: AGPL-3.0-or-later and Apache-2.0 with xAI amendments
+# AGPL-3.0-or-later
 # See above for full license details.
 import numpy as np
 from src.hash.spiral_hash import kappa_spiral_hash, proof_check
