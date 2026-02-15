a8012b2e52dc45ee837add5aa7775b19153ccb55
tetrasurfaces <sendtotoddhutchinson@gmail.com>
2026-02-15 20:09:54 +0900

    Update curve.c

--- b/KappashaOS/curve.c
+++ b/KappashaOS/curve.c
@@ -1,4 +1,19 @@
 // curve.c - Memory curve tool (Ubuntu port)
+// SPDX-License-Identifier: AGPL-3.0-or-later
+// Copyright (C) 2026 xAI
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/agpl-3.0.html>.
+//
+// Notes:
 // Compile: gcc -o curve curve.c -lm
 // Run: ./curve --note    or   ./curve test.txt
 
